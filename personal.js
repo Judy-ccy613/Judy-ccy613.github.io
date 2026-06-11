e4b19309f2b4e464a15164590a2958e66367b4ac
Judy-ccy613 <chenjudy0613@gapp.nthu.edu.tw>
2026-06-11 17:31:55 +0800

    Add files via upload

--- personal.js
+++ personal.js
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream
 //window.onload = function() {
 //	let em = document.querySelector("article > p");
 //	em3.style.color="white";}
 //console.log(document)
+=======
+//window.onload = function() {
+//	let em = document.querySelector("article > p");
+//	em3.style.color="white";}
+//console.log(document)
+>>>>>>> Stashed changes
